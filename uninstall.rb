464d0a98e42fe1085d3032cf1afb5abd22a60a7b
Andy Atkinson <andy@webandy.com>
2011-01-23 16:23:35 -0500

    remove the version append filename, don't remove the migration file, but provide user uninstall instructions

--- uninstall.rb
+++ uninstall.rb
@@ -1,4 +1,8 @@
-# cleanup generated files
+# provide uninstall instructions to user
 require 'fileutils'
-Dir.glob(File.join('db', 'migrate', '**')){|f| FileUtils.rm(f.grep(/_abingo_migration/)) }
+migration_file = Dir.glob(File.join('db', 'migrate', '**')){|f| f.grep(/_abingo_migration/) }
+puts "Abingo migration file #{migration_file} has not been modified. Run the down method manually to reverse the migration."
+
+stylesheet = File.join(Rails.root, 'public', 'stylesheets', 'abingo_dashboard.css')
+puts "Removing the abingo dashboard stylesheet file: #{stylesheet}"
 FileUtils.rm(File.join(Rails.root, 'public', 'stylesheets', 'abingo_dashboard.css'))
